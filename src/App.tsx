49d3d1d5d4ea16d1cf6159bfb88ec1a9ae3cb146
Yahya <ian.w.regs@gmail.com>
2021-09-11 14:12:10 +0100

    added drag and drop functionality to list

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd';
 import './App.css';
 
+const listItems = [
+  {
+    id: "1",
+    name: "study spanish"
+  },
+  {
+    id: "2",
+    name: "start workout"
+  },
+  {
+    id: "3",
+    name: "create dash component"
+  },
+  {
+    id: "5",
+    name: "Watch Netflix"
+  }
+]
+
+const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
+  padding: 10,
+  margin: `0 50px 15px 50px`,
+  background: isDragging ? "#4a2975" : "white",
+  color: isDragging ? "white" : "black",
+  border: `1px solid black`,
+
+  ...draggableStyle
+})
+
 function App() {
+
+  const [todo, setTodo] = useState(listItems)
+  const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result
+    if (!destination) return
+
+    const items = Array.from(todo)
+    const [newOrder] = items.splice(source.index, 1)
+    items.splice(destination.index, 0, newOrder)
+
+    setTodo(items)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Drag and Drop</h1>
+      <DragDropContext onDragEnd={onDragEnd} >
+
+        <Droppable droppableId="todo" >
+          {(provided) => (
+            <div className="todo" {...provided.droppableProps} ref={provided.innerRef}>
+              {todo.map(({ id, name }, index) => {
+                return (
+                  <Draggable key={id} draggableId={id} index={index} >
+                    {(provided, snapshot) => (
+                      <div ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
+                      >
+                        {name}
+                      </div>
+                    )}
+                  </Draggable>
+                )
+              })}
+            </div>
+          )}
+        </Droppable>
+
+      </DragDropContext>
+
+
     </div>
   );
 }
